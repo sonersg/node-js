30da264c083f31804a7b874721cb7c83753dc0f7
Soner <sonersg@outlook.com>
2024-10-01 00:07:39 +0300

    commonJS modules

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 // global object instead of window object
 console.log(global);
+
 // commonJS modules instead of ES6 modules
 const os = require("os");
+const path = require("path");
 
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+console.log(path.parse(__filename));
