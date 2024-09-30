014fb38edeaff30e8ff7ee4e88a0f2d0ab5a4537
Soner <sonersg@outlook.com>
2024-10-01 00:07:46 +0300

    commonJS modules

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ console.log(global);
 // commonJS modules instead of ES6 modules
 const os = require("os");
 const path = require("path");
+const { add, subtract, multiply, divide } = require("./math");
+
+console.log(add(23, 7));
 
 console.log(os.type());
 console.log(os.version());
@@ -16,3 +19,5 @@ console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 console.log(path.parse(__filename));
+
+// missing some JS APIs like fetch
