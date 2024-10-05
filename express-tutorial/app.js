a09e015a8d0762bdb9509a817b2539699a5018ae
Soner <sonersg@outlook.com>
2024-10-05 11:13:17 +0300

    server by http module

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const fs = require("fs");
 
-const homePage = fs.readFileSync("./index.html");
+const homePage = fs.readFileSync("./navbar-app/index.html");
+const styles = fs.readFileSync("./navbar-app/styles.css");
+const logo = fs.readFileSync("./navbar-app/logo.svg");
+const logic = fs.readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   console.log("user hit tthe server");
@@ -9,14 +12,38 @@ const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === "/") {
+    // home
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     res.end();
-  } else if (url === "/about") {
+  }
+
+  // about
+  else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
     res.write("<h1>about page</h1>");
     res.end();
-  } else {
+  }
+  // styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(styles);
+    res.end();
+  }
+  // logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(logo);
+    res.end();
+  }
+  // logic
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(logic);
+    res.end();
+  }
+  // 404 not found
+  else {
     res.writeHead(404, { "content-type": "text/html" });
     res.write("<h1>page not found</h1>");
     res.end();
